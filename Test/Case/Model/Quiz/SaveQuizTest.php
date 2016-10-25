3bb1cee593ea40f10241d8feaa6f8c01dbf40f68
s-nakajima <nakajimashouhei@gmail.com>
2016-10-25 11:04:26 +0900

    ボックスの切り替え機能追加に伴うUnitTestエラー修正 https://github.com/NetCommons3/NetCommons3/issues/726

--- Test/Case/Model/Quiz/SaveQuizTest.php
+++ Test/Case/Model/Quiz/SaveQuizTest.php
@@ -72,7 +72,7 @@ class QuizSaveQuizTest extends WorkflowSaveTest {
 		$this->$model->Behaviors->unload('AuthorizationKey');
 		Current::$current['Frame']['id'] = '6';
 		Current::$current['Frame']['key'] = 'frame_3';
-		Current::$current['Frame']['room_id'] = '1';
+		Current::$current['Frame']['room_id'] = '2';
 		Current::$current['Frame']['plugin_key'] = 'quizzes';
 		Current::$current['Frame']['language_id'] = '2';
 		Current::$current['Plugin']['key'] = 'quizzes';
